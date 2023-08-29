f44a43800df1614cf93eb100ecdf941b6de961d7
Jabir <mjabir.t@gmail.com>
2023-08-29 21:18:21 +0530

    next deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin, faOrcid } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-
+import { ContentRoutingModule } from './content-component/content-routing.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponentComponent } from './profile-component/profile-component.component';
 import { NamePicComponentComponent } from './ProfileComponent/name-pic-component/name-pic-component.component';
 import { InfoComponentComponent } from './ProfileComponent/info-component/info-component.component';
 import { ContentComponentComponent } from './content-component/content-component.component';
-import { ContentRoutingModule } from './content-component/content-routing.module';
+
 
 import { ResearchComponent } from './ContentComponent/research/research.component';
 import { TechnicalExpertiseComponent } from './ContentComponent/technical-expertise/technical-expertise.component';
